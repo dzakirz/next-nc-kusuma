cbf0353af90bc12d346869d05fb1e518bdd1911b
dzakirz <dzakirz.dev@gmail.com>
2023-02-28 19:25:03 +0700

    reset and add dependencies (framer-motion and tailwind)

---
+++ types/next-auth.d.ts
@@ -0,0 +1,15 @@
+import NextAuth, { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      role: string;
+    } & DefaultSession["user"];
+  }
+  interface User extends DefaultUser {
+    role: string;
+  }
+}
