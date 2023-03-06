d43f111a36ffd8153406b7779d22f963cbb2960d
dzakirz <dzakirz.dev@gmail.com>
2023-03-06 10:42:08 +0700

    fix button color bug and add button constant

---
+++ components/buttons/constants.ts
@@ -0,0 +1,20 @@
+export const Styles = {
+  normal: {
+    primary: "py-3 mt-2 text-base font-semibold shadow-lg lg:py-2 lg:text-lg text-opacity-70 rounded-xl bg-primary hover:bg-primary-focus",
+    secondary: "py-3 mt-2 text-base font-semibold shadow-lg lg:py-2 lg:text-lg text-opacity-70 rounded-xl bg-secondary hover:bg-secondary-focus",
+    accent: "py-3 mt-2 text-base font-semibold shadow-lg lg:py-2 lg:text-lg text-opacity-70 rounded-xl bg-accent hover:bg-accent-focus",
+    success: "py-3 mt-2 text-base font-semibold shadow-lg lg:py-2 lg:text-lg text-opacity-70 rounded-xl bg-success hover:bg-success-focus",
+    error: "py-3 mt-2 text-base font-semibold shadow-lg lg:py-2 lg:text-lg text-opacity-70 rounded-xl bg-error hover:bg-error-focus",
+    warning: "py-3 mt-2 text-base font-semibold shadow-lg lg:py-2 lg:text-lg text-opacity-70 rounded-xl bg-warning hover:bg-warning-focus",
+    info: "py-3 mt-2 text-base font-semibold shadow-lg lg:py-2 lg:text-lg text-opacity-70 rounded-xl bg-primary hover:bg-primary-focus",
+  },
+  leftIcon: {
+    primary: `flex items-center justify-center gap-2 py-3 text-base font-semibold shadow-lg cursor-pointer lg:py-2 text-opacity-70 lg:text-lg rounded-xl bg-primary hover:bg-primary-focus`,
+    secondary: `flex items-center justify-center gap-2 py-3 text-base font-semibold shadow-lg cursor-pointer lg:py-2 text-opacity-70 lg:text-lg rounded-xl bg-secondary hover:bg-secondary-focus`,
+    accent: `flex items-center justify-center gap-2 py-3 text-base font-semibold shadow-lg cursor-pointer lg:py-2 text-opacity-70 lg:text-lg rounded-xl bg-accent hover:bg-accent-focus`,
+    success: `flex items-center justify-center gap-2 py-3 text-base font-semibold shadow-lg cursor-pointer lg:py-2 text-opacity-70 lg:text-lg rounded-xl bg-success hover:bg-success-focus`,
+    error: `flex items-center justify-center gap-2 py-3 text-base font-semibold shadow-lg cursor-pointer lg:py-2 text-opacity-70 lg:text-lg rounded-xl bg-error hover:bg-error-focus`,
+    warning: `flex items-center justify-center gap-2 py-3 text-base font-semibold shadow-lg cursor-pointer lg:py-2 text-opacity-70 lg:text-lg rounded-xl bg-warning hover:bg-warning-focus`,
+    info: `flex items-center justify-center gap-2 py-3 text-base font-semibold shadow-lg cursor-pointer lg:py-2 text-opacity-70 lg:text-lg rounded-xl bg-info hover:bg-info-focus`,
+  }
+}
