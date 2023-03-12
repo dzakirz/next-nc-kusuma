9a038369ce6f6065ec744afacda78a4aab89bfb4
dzakirz <dzakirz.dev@gmail.com>
2023-03-12 17:32:23 +0700

    final reset

--- lib/context/index.ts
+++
@@ -1,4 +0,0 @@
-import SidebarProvider from './sidebar-context'
-import { sidebarContext } from './sidebar-context'
-
-export { SidebarProvider, sidebarContext }
--- types/props.d.ts
+++
@@ -1,3 +0,0 @@
-export type ComponentPropsChildren = {
-  children: React.ReactNode;
-};
