0daa8c6c9e737c8566eb9f8261d932185c5b2e28
dzakirz <dzakirz.dev@gmail.com>
2023-03-12 17:31:00 +0700

    last step before remove all

---
+++ lib/context/index.ts
@@ -0,0 +1,4 @@
+import SidebarProvider from './sidebar-context'
+import { sidebarContext } from './sidebar-context'
+
+export { SidebarProvider, sidebarContext }
---
+++ types/props.d.ts
@@ -0,0 +1,3 @@
+export type ComponentPropsChildren = {
+  children: React.ReactNode;
+};
